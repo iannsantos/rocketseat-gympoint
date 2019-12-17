6f7a623f00ba96017da57e23d89017541a4dffbf
Iann Santos <iannsantos8@gmail.com>
2019-12-16 23:11:32 -0300

    add routes

---
+++ frontend/src/routes/Route.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { store } from '~/store';
+
+import AuthLayout from '~/pages/_layouts/auth';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const { signed } = store.getState().auth;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/main" />;
+  }
+
+  const Layout = signed ? AuthLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false
+};
