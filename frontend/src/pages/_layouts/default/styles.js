9385dbce14cb2ffd9041d02d9acfe760bc614058
Iann Santos <iannsantos8@gmail.com>
2019-12-18 22:28:40 -0300

    add default layout

---
+++ frontend/src/pages/_layouts/default/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100%;
+  padding-bottom: 15px;
+  background: #f2f2f2;
+`;
+
+export const Content = styled.div``;
